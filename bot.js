e18afc85d8735016d4f9ae5e7a4c3f03e2cc1ccf
khaskhoussi1 <44135131+khaskhoussi1@users.noreply.github.com>
2018-10-15 16:18:34 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -25,35 +25,5 @@ client.user.setGame(` I ❤ ..  `,"http://twitch.tv/S-F")
   console.log('')
   console.log('')
 });
- 
- 
-client.on("message", message => {
-    var prefix = "-";
-        if (message.author.id === client.user.id) return;
-        if (message.guild) {
-       let embed = new Discord.RichEmbed()
-        let args = message.content.split(' ').slice(1).join(' ');
-    if(message.content.split(' ')[0] == prefix + 'bc') {
-        if (!args[1]) {
-    message.channel.send("bc <message>");
-    return;
-    }
-            message.guild.members.forEach(m => {
-       if(!message.member.hasPermission('ADMINISTRATOR')) return;
-                m.send(args);
-            });
-            const AziRo = new Discord.RichEmbed()
-            .setAuthor(message.author.username, message.author.avatarURL)
-            .setTitle(':white_check_mark:| جاري ارسال رسالتك ') 
-            .addBlankField(true)
-            .addField(':hotsprings:| عدد الاعضاء المرسل لهم ', message.guild.memberCount , true)
-            .addField(':pencil:| الرسالة ', args)
-            .setColor('RANDOM')
-            message.channel.sendEmbed(AziRo);
-        }
-        } else {
-            return;
-        }
-    });
 
 client.login(process.env.BOT_TOKEN);
